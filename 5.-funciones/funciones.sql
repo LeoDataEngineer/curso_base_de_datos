8314fbfc311c4ef45c39d7965c6e380bd4a3cea3
LeoDataEngineer <116231124+LeoDataEngineer@users.noreply.github.com>
2024-04-25 21:44:56 -0300

    Update funciones.sql

--- 5.-funciones/funciones.sql
+++ 5.-funciones/funciones.sql
@@ -3,45 +3,41 @@ DELIMITER //
 
 CREATE FUNCTION agregar_dias(fecha DATE, dias INT)
 RETURNS DATE
--- DETERMINISTIC -- agragar esta linea en mi version de mysql
+DETERMINISTIC
 BEGIN
   RETURN fecha + INTERVAL dias DAY;
-END//
+END //
 
+DELIMITER ;
+  
 DELIMITER //  
 CREATE FUNCTION obtener_paginas()
 RETURNS INT
 DETERMINISTIC
 BEGIN
-  SET @paginas = (SELECT (ROUND( RAND() * 100 ) * 4 ));
-  RETURN @paginas;
+  DECLARE paginas INT; -- Declarar la variable local
+  SET paginas = (SELECT (ROUND( RAND() * 100 ) * 4 )); -- Asignar el valor a la variable
+  RETURN paginas; -- Devolver el valor de la variable
 END//
 
 DELIMITER ;
   
-Esta modifica por mi version de mysql
-CREATE FUNCTION obtener_paginas()
-RETURNS INT
-DETERMINISTIC
-BEGIN
-  DECLARE paginas INT;
-  SET paginas = (SELECT ROUND(RAND() * 100) * 4);
-  RETURN paginas;
-END//
-
-
+DELIMITER //
 CREATE FUNCTION obtener_ventas()
 RETURNS INT
+DETERMINISTIC  
 BEGIN
-  SET @paginas = (SELECT (ROUND( RAND() * 100 ) * 6 ));
-  RETURN @paginas;
+  DECLARE ventas INT; -- Declarar la variable local
+  SET ventas = (SELECT (ROUND( RAND() * 100 ) * 6 ));
+  RETURN ventas;
 END//
 
 DELIMITER ;
 
 UPDATE libros SET paginas = obtener_paginas();
 UPDATE libros SET ventas = obtener_ventas();
+
+
 -- para ver mis funciones en la version de mi mysql
-SELECT ROUTINE_NAME
-FROM information_schema.routines
-WHERE ROUTINE_SCHEMA = 'libreria_cf' AND ROUTINE_TYPE = 'FUNCTION';
+SHOW FUNCTION STATUS WHERE Db = 'libreria_cf';
+
